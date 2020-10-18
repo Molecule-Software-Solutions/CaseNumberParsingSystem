261b2cde3e7e75e062c116094121453daba7b223
joshman1019 <36560776+joshman1019@users.noreply.github.com>
2020-10-18 01:59:09 -0400

    Attempt to correct sequence blanking bug

--- CaseNumberParser/ParseEngine.cs
+++ CaseNumberParser/ParseEngine.cs
@@ -59,11 +59,11 @@ namespace CaseNumberParser
                     switch (caseNumberFormat)
                     {
                         case CaseNumberFormat.OneDigitCaseType:
-                            return input.Substring(5).PadLeft(6, '0'); 
+                            return input.Substring(6).PadLeft(6, '0'); 
                         case CaseNumberFormat.TwoDigitCaseType:
-                            return input.Substring(5).PadLeft(7, '0');
+                            return input.Substring(7).PadLeft(6, '0');
                         case CaseNumberFormat.ThreeDigitCaseType:
-                            return input.Substring(5).PadLeft(8, '0');
+                            return input.Substring(8).PadLeft(6, '0');
                         default:
                             return string.Empty; 
                     }
@@ -71,11 +71,11 @@ namespace CaseNumberParser
                     switch (caseNumberFormat)
                     {
                         case CaseNumberFormat.OneDigitCaseType:
-                            return input.Substring(7).PadLeft(8, '0'); 
+                            return input.Substring(8).PadLeft(8, '0'); 
                         case CaseNumberFormat.TwoDigitCaseType:
-                            return input.Substring(7).PadLeft(9, '0');
+                            return input.Substring(9).PadLeft(9, '0');
                         case CaseNumberFormat.ThreeDigitCaseType:
-                            return input.Substring(7).PadLeft(10, '0');
+                            return input.Substring(10).PadLeft(10, '0');
                         default:
                             return string.Empty; 
                     }
